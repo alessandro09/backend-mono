867fb966b39e0b266cd2b81f26478c3ccc06312a
Alessandro Abegg <alessandro.abegg@gmail.com>
2026-08-29 21:12:32 -0300

    add login controller and security configuration for custom login page

---
+++ src/main/java/br/com/alessandro/backend/auth/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package br.com.alessandro.backend.auth.config;
+
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.MediaType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.util.matcher.MediaTypeRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Bean
+	@Order(Ordered.HIGHEST_PRECEDENCE)
+	SecurityFilterChain authorizationServerSecurityFilterChain(HttpSecurity http) {
+		http.oauth2AuthorizationServer((authorizationServer) -> {
+			http.securityMatcher(authorizationServer.getEndpointsMatcher());
+			authorizationServer.oidc(withDefaults());
+		});
+		http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated());
+		http.oauth2ResourceServer((resourceServer) -> resourceServer.jwt(withDefaults()));
+		http.exceptionHandling((exceptions) -> exceptions.defaultAuthenticationEntryPointFor(
+				new LoginUrlAuthenticationEntryPoint("/login"), createRequestMatcher()));
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) {
+		http.authorizeHttpRequests((authorize) -> authorize.anyRequest().authenticated())
+			.formLogin((formLogin) -> formLogin.loginPage("/login").permitAll());
+		return http.build();
+	}
+
+	private static RequestMatcher createRequestMatcher() {
+		MediaTypeRequestMatcher requestMatcher = new MediaTypeRequestMatcher(MediaType.TEXT_HTML);
+		requestMatcher.setIgnoredMediaTypes(Set.of(MediaType.ALL));
+		return requestMatcher;
+	}
+
+}
